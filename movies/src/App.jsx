317f49944894ce69b1dce8883a9d532b3a0dec4f
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-03-26 11:58:40 -0500

    Update App.jsx

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css'
-import { Nav } from './components/Nav'
-import { Searcher } from './components/Searcher'
-import { Fetch } from './services/Fetch'
+import "./App.css";
+import { Nav } from "./components/Nav";
+import { Searcher } from "./components/Searcher";
+import { Fetch } from "./services/Fetch";
+import { BroserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div>
-      {/* <Searcher /> */}
-      {/* <Fetch /> */}
-      <Nav />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" />
+          <Route path="/" />
+          <Route path="/" />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
