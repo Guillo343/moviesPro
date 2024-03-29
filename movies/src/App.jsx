a35be214e787ecd3f34a79636b14e6b4a96b2ec9
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-03-29 15:31:09 -0500

    Update App.jsx

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -3,6 +3,8 @@ import { Nav } from "./components/Nav";
 import { Searcher } from "./components/Searcher";
 import { Fetch } from "./services/Fetch";
 import {Home} from './pages/Home'
+import {Movies} from './pages/Movies'
+import {Series} from './pages/Series'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
         <Searcher />
         <Routes>
           <Route path="/Home" index element={<Home />} />
-          <Route path="/Movies" />
-          <Route path="/Series" />
+          <Route path="/Movies" element={<Movies />} />
+          <Route path="/Series" element={<Series />} />
         </Routes>
       </Router>
     </div>
