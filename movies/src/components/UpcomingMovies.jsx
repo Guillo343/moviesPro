c58d61549fbdcc8c33e096a7aa6550ab53e1a1dd
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-04-04 14:04:29 -0500

    Update UpcomingMovies.jsx

--- movies/src/components/UpcomingMovies.jsx
+++ movies/src/components/UpcomingMovies.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../../public/UpcomingMovies.css';
+// import '../../public/UpcomingMovies.css';
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 export const UpcomingMovies = () => {
