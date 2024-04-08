7e2a4c2b63dcd702d650d4f791d6afbdd6c4f96b
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-04-08 11:33:07 -0500

    Summary

--- movies/src/components/Nav.jsx
+++ movies/src/components/Nav.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../../public/Navbar.css'
 import Logo from '../assets/logo.png'
 import { Searcher } from './Searcher'
 
 export const Nav = () => {
+  const [activeLink, setActiveLink] = useState('Movies');
 
+
+  const handleLogoClick = () =>{
+    setActiveLink('Movies')
+  }
   
   return (
     <nav className='navbar'>
     <div className='nav-div'>
-      <Link to='/Movies' className='nav-logo'><img src={Logo} alt="logo" /></Link>
+      <Link to='/Movies' className='nav-logo' onClick={handleLogoClick}><img src={Logo} alt="logo" /></Link>
       <ul className='nav-ul'>
-        <li><Link to='/Movies' className='nav-li'>Movies</Link></li>
-        <li><Link to='/Series' className='nav-li'>TV Series</Link></li>
+        <li className={activeLink === 'Movies' ? 'active' : ''}>
+          <Link to='/Movies' className='nav-li' onClick={() => setActiveLink('Movies')}>Movies</Link>
+        </li>
+        <li className={activeLink === 'TV Series' ? 'active' : '' }>
+          <Link to='/Series' className='nav-li' onClick={() => setActiveLink('TV Series')}>TV Series</Link>
+        </li>
       </ul>
       <Searcher />
     </div>
